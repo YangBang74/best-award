b54523ebdb0a9e8551562762f90bdc12d64a5f0a
Yang Bang74 <crowbeat.47@gmail.com>
2025-05-07 16:32:34 +0500

    fix: fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import vueI18n from 'vue-i18n'
 
 // https://vite.dev/config/
 export default defineConfig({
