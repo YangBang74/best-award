c20d434fe79d0ee8a9ddd3f1ac6ea3b1d512b6b5
Yang Bang74 <crowbeat.47@gmail.com>
2025-05-07 16:35:09 +0500

    fix: fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vueI18n from 'vue-i18n'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    vueI18n({
-      include: path.resolve(__dirname, './src/locales/**'),
-    }),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
